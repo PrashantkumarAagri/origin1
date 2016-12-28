9e25b52199417390b05acecb077bec0a9aecae01
PrashantkumarAagri <aagriprashant@gmail.com>
2016-12-28 16:36:55 +0545

    adding form

---
+++ form.php
@@ -0,0 +1,45 @@
+<!doctype html>
+	<html>
+	 	<head>
+	 		<title>new project</title>
+	 		<link href="style.css" type="text/css" rel="stylesheet">
+	 	</head>
+	 		<body>
+
+	 			
+	 				<table style="width:100%">
+						  <tr>
+						    <th>Firstname</th>
+						    <th>Lastname</th> 
+						    <th>Age</th>
+						  </tr>
+						  <tr>
+						    <td>Jill</td>
+						    <td>Smith</td> 
+						    <td>50</td>
+						  </tr>
+						  <tr>
+						    <td>Eve</td>
+						    <td>Jackson</td> 
+						    <td>94</td>
+						  </tr>
+					</table>
+	 				
+
+
+
+	 		
+
+
+
+
+
+
+
+	 		</body>
+
+	 			<footer>
+	 				
+	 			</footer>
+	 		
+	</html>  
